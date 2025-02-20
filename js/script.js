87e07b08ad3749c96f8d02cc4ce5e8fcd73552f3
bibashmanjusubedi <acharabibash@outlook.com>
2025-02-20 10:26:32 +0545

    Crop part of Carausel for Dojo3.jpg

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     const showcase = document.getElementById("showcase");
     
-    // Array of background images
     const images = [
-        "./img/freelancer.jpg",
-        "./img/showcase2.jpg",
-        "./img/coding.jpg"
+        "./img/Dojo2.jpg",
+        "./img/Dojo1.jpg",
+        "./img/Dojo3.jpg"
     ];
     
     let index = 0;
 
     function changeBackground() {
         showcase.style.backgroundImage = `url('${images[index]}')`;
+        
+        // Apply clip-path and center only for Dojo3.jpg
+        if (images[index].includes("Dojo3.jpg")) {
+            showcase.style.clipPath = "inset(10% 0 0 0)"; // Crop 10% from top and bottom
+            showcase.style.backgroundPosition = "center"; // Center the image
+        } else {
+            showcase.style.clipPath = "none"; // Reset clip-path for other images
+            showcase.style.backgroundPosition = "center"; // Optionally center other images
+        }
+
         index = (index + 1) % images.length; // Loop through images
     }
 
