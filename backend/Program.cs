13e3b5f3d2bbb1259742fb5d2a89b39c151d0ec2
bibashmanjusubedi <acharabibash@outlook.com>
2025-09-20 17:02:49 +0545

    uses https

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(); 
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+
 app.MapControllers();
 app.MapGet("/", () => "Hello World!");
 
